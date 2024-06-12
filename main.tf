f436d2011e98813486651f56baf9f2b1b9578864
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-06-12 06:19:18 -0600

    Fixing formatting

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "sdm_node" "this" {
 
 data "aws_iam_policy_document" "assume" {
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
     principals {
       identifiers = ["ec2.amazonaws.com"]
@@ -30,39 +30,39 @@ data "aws_iam_policy_document" "assume" {
 }
 
 resource "aws_iam_role" "this" {
-  count = var.create && var.enable_ssm ? 1 : 0
-  name = "${var.name}${var.instance_suffix}-role"
+  count              = var.create && var.enable_ssm ? 1 : 0
+  name               = "${var.name}${var.instance_suffix}-role"
   assume_role_policy = data.aws_iam_policy_document.assume.json
 }
 
 resource "aws_iam_role_policy_attachment" "ssm" {
-  count = var.create && var.enable_ssm ? 1 : 0
+  count      = var.create && var.enable_ssm ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.this[count.index].name
 }
 
 resource "aws_iam_instance_profile" "this" {
   count = var.create && var.enable_ssm ? 1 : 0
-  name = "${var.name}${var.instance_suffix}-instance-profile"
-  path = "/"
-  role = aws_iam_role.this[count.index].name
+  name  = "${var.name}${var.instance_suffix}-instance-profile"
+  path  = "/"
+  role  = aws_iam_role.this[count.index].name
 }
 
 resource "aws_security_group" "this" {
-  count = var.create ? 1 : 0
-  name = "${var.name}${var.instance_suffix}"
+  count  = var.create ? 1 : 0
+  name   = "${var.name}${var.instance_suffix}"
   vpc_id = var.vpc_id
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = var.ssh_allowed_cidr_blocks
   }
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = -1
-    cidr_blocks = var.egress_cidr_blocks
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = var.egress_cidr_blocks
     ipv6_cidr_blocks = var.egress_cidr_blocks_ipv6
   }
   tags = merge(var.security_group_tags, merge(var.tags, {
@@ -71,25 +71,25 @@ resource "aws_security_group" "this" {
 }
 
 resource "aws_instance" "this" {
-  count = var.create ? 1 : 0
-  ami = var.ami == null ? data.aws_ami.this.id : var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.this[count.index].id]
-  subnet_id = var.subnet_id
+  count                   = var.create ? 1 : 0
+  ami                     = var.ami == null ? data.aws_ami.this.id : var.ami
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = [aws_security_group.this[count.index].id]
+  subnet_id               = var.subnet_id
   disable_api_termination = false
-  key_name = var.key_name
-  iam_instance_profile = join("", aws_iam_instance_profile.this.*.name)
+  key_name                = var.key_name
+  iam_instance_profile    = join("", aws_iam_instance_profile.this.*.name)
   root_block_device {
     delete_on_termination = true
-    encrypted = true
-    volume_type = "gp3"
-    volume_size = var.root_volume_size
+    encrypted             = true
+    volume_type           = "gp3"
+    volume_size           = var.root_volume_size
   }
   user_data_replace_on_change = true
   user_data = templatefile("${path.module}/init.sh.tpl", {
     identifier = var.force_deploy ? timestamp() : 0
-    ssh_keys = var.ssh_keys
-    token = sdm_node.this[count.index].relay[0].token
+    ssh_keys   = var.ssh_keys
+    token      = sdm_node.this[count.index].relay[0].token
   })
   depends_on = [sdm_node.this]
   tags = merge(var.instance_tags, merge(var.tags, {
--- variables.tf
+++ variables.tf
@@ -1,116 +1,116 @@
 variable "name" {
-  type = string
+  type        = string
   description = "Name of the relay"
 }
 
 variable "create" {
-  default = true
-  type = bool
+  default     = true
+  type        = bool
   description = "Set to false to not create any resources in this stack"
 }
 
 variable "instance_type" {
-  default = "t4g.small"
-  type = string
+  default     = "t4g.small"
+  type        = string
   description = "EC2 instance type to use for the gateway"
 }
 
 variable "ssh_allowed_cidr_blocks" {
-  default = []
-  type = list(string)
+  default     = []
+  type        = list(string)
   description = "Default allowed CIDRs to access SSH on this EC2 instance"
 }
 
 variable "egress_cidr_blocks" {
-  default = ["0.0.0.0/0"]
-  type = list(string)
+  default     = ["0.0.0.0/0"]
+  type        = list(string)
   description = "Default allowed CIDRs for egress"
 }
 
 variable "egress_cidr_blocks_ipv6" {
-  default = ["::/0"]
-  type = list(string)
+  default     = ["::/0"]
+  type        = list(string)
   description = "Default allowed CIDRs for egress"
 }
 
 variable "vpc_id" {
-  type = string
+  type        = string
   description = "ID of the VPC to provision the EC2 instance in"
 }
 
 variable "subnet_id" {
-  type = string
+  type        = string
   description = "The subnet to provision the EC2 instance in"
 }
 
 variable "ami" {
-  default = null
-  type = string
+  default     = null
+  type        = string
   description = "Optional AMI to use. Default is to use the latest Ubuntu 20.04 amd64 image"
 }
 
 variable "key_name" {
-  default = null
-  type = string
+  default     = null
+  type        = string
   description = "SSH key pair to use"
 }
 
 variable "ssh_keys" {
-  default = []
-  type = list(string)
+  default     = []
+  type        = list(string)
   description = "List of SSH public keys to be added to the authorized_keys file"
 }
 
 variable "tags" {
-  default = {}
-  type = map(string)
+  default     = {}
+  type        = map(string)
   description = "Additional tags for all resources"
 }
 
 variable "instance_tags" {
-  default = {}
-  type = map(string)
+  default     = {}
+  type        = map(string)
   description = "Additional tags for the EC2 instance"
 }
 
 variable "security_group_tags" {
-  default = {}
-  type = map(string)
+  default     = {}
+  type        = map(string)
   description = "Additional tags for the Security group"
 }
 
 variable "root_volume_size" {
-  default = 20
-  type = number
+  default     = 20
+  type        = number
   description = "Size of the root volume"
 }
 
 variable "enable_ssm" {
-  type = bool
-  default = true
+  type        = bool
+  default     = true
   description = "Enable or disable SSM on this instance"
 }
 
 variable "instance_suffix" {
-  type = string
-  default = "-strongdm"
+  type        = string
+  default     = "-strongdm"
   description = "The suffix to use on the name when creating the EC2 instance"
 }
 
 variable "ami_name_filters" {
-  type = list(string)
-  default = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*"]
+  type        = list(string)
+  default     = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*"]
   description = "Name filter to use when searching for AMIs"
 }
 
 variable "ami_owners" {
-  type = list(string)
-  default = ["099720109477"]
+  type        = list(string)
+  default     = ["099720109477"]
   description = "Owner filter to use when searching for AMIs"
 }
 
 variable "force_deploy" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Forcibly re-deploy the EC2 instance."
 }
